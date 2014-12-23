f3aa66a0b73e10894a8989c8521c327ce84e19e2
vertoe <vertoe@qhor.net>
2014-12-23 01:26:53 +0100

    Add masternode dummy window.

---
+++ mnode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import curses, time
+
+import global_mod as g
+import footer
+
+def draw_window(state, window):
+    window.clear()
+    window.refresh()
+
+    win_header = curses.newwin(3, 75, 0, 0)
+
+    if 'masternodes' in state:
+        win_header.addstr(0, 1, "masternode information successfully loaded", curses.A_BOLD + curses.color_pair(1))
+
+    else:
+        win_header.addstr(0, 1, "under construction", curses.A_BOLD + curses.color_pair(3))
+
+    win_header.refresh()
+    footer.draw_window(state)
